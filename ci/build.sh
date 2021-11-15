211f389a879cc6a5048f3751c7ee000e2a2a1bf2
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-15 22:00:41 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -5,9 +5,9 @@ cd "$(dirname "$0")/.."
 source ci/env.sh
 source ci/upload-ci-artifact.sh
 # sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install freeglut3-dev
-sudo apt-get -y install cuda cuda-10-1 cuda-toolkit-10-1 cuda-samples-10-1 cuda-documentation-10-1
+sudo apt-get update -y
+sudo apt-get install freeglut3-dev -y
+sudo apt-get -y install cuda cuda-10-1 cuda-toolkit-10-1 cuda-samples-10-1 cuda-documentation-10-1 -y
 # sudo apt-get install wget
 # sudo apt install software-properties-common -y
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
