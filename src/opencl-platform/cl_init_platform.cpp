bfa428530e5c966e656ec3e45a4d20fe7bcfa0d8
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-15 15:12:48 +0530

    Update cl_init_platform.cpp

--- src/opencl-platform/cl_init_platform.cpp
+++ src/opencl-platform/cl_init_platform.cpp
@@ -6,7 +6,9 @@
 #include <string>
 #include <fstream>
 #include "cl_common.h"
-#include <CL/cl.h>  #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define CL_TARGET_OPENCL_VERSION 220
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#include <CL/cl.h>
 cl_uint query_device_type = CL_DEVICE_TYPE_ALL;
 bool cl_is_init = false;
 
