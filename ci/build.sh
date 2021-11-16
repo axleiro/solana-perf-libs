ce2edb7820dc93dc005e7faefd07cdef03418a88
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-16 15:29:17 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -4,23 +4,6 @@ cd "$(dirname "$0")/.."
 
 source ci/env.sh
 source ci/upload-ci-artifact.sh
-# sudo apt clean
-# sudo apt update
-# sudo apt purge nvidia-* 
-# sudo apt autoremove
-# sudo apt install -y cuda
-# sudo add-apt-repository universe
-# sudo apt-get update -y
-# sudo apt-get install freeglut3-dev -y
-# sudo apt-get -y install cuda cuda-10-1 cuda-toolkit-10-1 cuda-samples-10-1 cuda-documentation-10-1 -y
-# sudo apt-get install wget
-# sudo apt install software-properties-common -y
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-# sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-# sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-# sudo apt-get update
-# sudo apt-get -y install cuda
 nvcc --version
 CUDA_HOMES=(
   /usr/local/cuda-10.0
@@ -41,9 +24,7 @@ for CUDA_HOME in "${CUDA_HOMES[@]}"; do
     export LD_LIBRARY_PATH=$CUDA_HOME/lib64
     export PATH=$PATH:$HOME/.cargo/bin/:$CUDA_HOME/bin
     export DESTDIR=dist/$CUDA_HOME_BASE
-
-#     make -j"$(nproc)"
-    make -j32
+    make -j"$(nproc)"
     make install
     make clean
 
