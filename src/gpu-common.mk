33426d2f6f07541749113fe2c16c87aad0662a65
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-17 14:59:28 +0530

    Update gpu-common.mk

--- src/gpu-common.mk
+++ src/gpu-common.mk
@@ -1,11 +1,7 @@
 NVCC:=nvcc
 #GPU_PTX_ARCH:=compute_35
-#GPU_PTX_ARCH:=compute_50
 GPU_PTX_ARCH:=compute_60
-#GPU_ARCHS?=sm_50,sm_61,sm_70,sm_80
-#GPU_ARCHS?=sm_52,sm_61,sm_70
 GPU_ARCHS?=sm_61,sm_70,sm_75,sm_80,sm_86
-#GPU_ARCHS?=sm_80
 HOST_CFLAGS:=-Wall -Werror -fPIC -Wno-strict-aliasing
 GPU_CFLAGS:=--gpu-code=$(GPU_ARCHS),$(GPU_PTX_ARCH) --gpu-architecture=$(GPU_PTX_ARCH)
 
