a9614c00a94d92864496fa1c05dbce0f610e2e1f
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-17 13:14:06 +0530

    Update gpu-common.mk

--- src/gpu-common.mk
+++ src/gpu-common.mk
@@ -1,7 +1,7 @@
 NVCC:=nvcc
 #GPU_PTX_ARCH:=compute_35
 #GPU_PTX_ARCH:=compute_50
-GPU_PTX_ARCH:=compute_80
+GPU_PTX_ARCH:=compute_60
 #GPU_ARCHS?=sm_50,sm_61,sm_70,sm_80
 #GPU_ARCHS?=sm_52,sm_61,sm_70
 GPU_ARCHS?=sm_61,sm_70,sm_75,sm_80,sm_86
