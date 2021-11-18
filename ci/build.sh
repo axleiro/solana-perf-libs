f1244fb133e68d7162052c0707829d95d4b1dbda
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-18 17:13:07 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -4,38 +4,39 @@ cd "$(dirname "$0")/.."
 
 source ci/env.sh
 source ci/upload-ci-artifact.sh
-# nvcc --version
-CUDA_HOMES=(
-  /usr/local/cuda-10.0
-  /usr/local/cuda-10.1
-  /usr/local/cuda-10.2
-#   /usr/local/cuda-11.2
-)
+# # nvcc --version
+# CUDA_HOMES=(
+#   /usr/local/cuda-10.0
+#   /usr/local/cuda-10.1
+#   /usr/local/cuda-10.2
+# #   /usr/local/cuda-11.2
+# )
 
-for CUDA_HOME in "${CUDA_HOMES[@]}"; do
-  CUDA_HOME_BASE="$(basename "$CUDA_HOME")"
-  echo "--- Build: $CUDA_HOME_BASE"
-  (
-    if [[ ! -d $CUDA_HOME/lib64 ]]; then
-      echo "Invalid CUDA_HOME: $CUDA_HOME"
-      exit 1
-    fi
+# for CUDA_HOME in "${CUDA_HOMES[@]}"; do
+#   CUDA_HOME_BASE="$(basename "$CUDA_HOME")"
+#   echo "--- Build: $CUDA_HOME_BASE"
+#   (
+#     if [[ ! -d $CUDA_HOME/lib64 ]]; then
+#       echo "Invalid CUDA_HOME: $CUDA_HOME"
+#       exit 1
+#     fi
 
-    set -x
-    export LD_LIBRARY_PATH=$CUDA_HOME/lib64
-    export PATH=$PATH:$HOME/.cargo/bin/:$CUDA_HOME/bin
-    export DESTDIR=dist/$CUDA_HOME_BASE
-    make -j"$(nproc)"
-    make install
-    make clean
+#     set -x
+#     export LD_LIBRARY_PATH=$CUDA_HOME/lib64
+#     export PATH=$PATH:$HOME/.cargo/bin/:$CUDA_HOME/bin
+#     export DESTDIR=dist/$CUDA_HOME_BASE
+#     make -j"$(nproc)"
+#     make install
+#     make clean
 
-    cp -vf "$CUDA_HOME"/version.txt "$DESTDIR"/cuda-version.txt
-  )
-done
+#     cp -vf "$CUDA_HOME"/version.txt "$DESTDIR"/cuda-version.txt
+#   )
+# done
 
 echo --- Build SGX
 (
   set -x
+  sudo dockerd
   ci/docker-run.sh solanalabs/sgxsdk src/sgx-ecc-ed25519/build.sh
   ci/docker-run.sh solanalabs/sgxsdk src/sgx/build.sh
 )
